fd9fd6b58fcdfb753d3dbee6bcaa0a55a307b9a3
antony-tha-king <mr.antony254@gmail.com>
2025-03-22 19:32:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.owner = process.env.OWNER_NUMBER || "2347036214381"
 global.botname = process.env.BOT_NAME || "αrch md"
 global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
 global.prefix = process.env.PREFIX || "."; // your desired prefix symbol only
-global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
+global.timezone = process.env.TIME_ZONE || "Africa/Nairobi";
 global.author = process.env.AUTHOR  || "Ednut";
 global.packname = process.env.PACK_NAME  || "Arch Md ²⁵";
 global.startup = process.env.STARTUP_MSG === 'true';
@@ -27,7 +27,7 @@ global.onlygroup = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work di
 
 global.simbol = "♘"
 global.tempatDB = 'database.json' // dont change might cause big errors 
-global.pairing_code = false // if deploying on panel set true if on any other platform leave it false
+global.pairing_code = true // if deploying on panel set true if on any other platform leave it false
 
 // Settings Image Url
 global.image = {
